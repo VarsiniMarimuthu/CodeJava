9d1fa15efbdf946525c577eca841df9d3ddca2c6
Varsini M <165877162+VarsiniMarimuthu@users.noreply.github.com>
2025-01-26 22:21:11 +0530

    Delete practice directory

--- practice/output.java
+++
@@ -1,9 +0,0 @@
-class output{
-    public static void main(String[] args){
-        int a=3,b=4,c;
-        c= a + b + a++ + b++ + ++a + ++b;
-        System.out.println("a:" +a);
-        System.out.println("b:" +b);
-        System.out.println("c:" +c);
-    }
-}
